8a0bb65b531f3bcb03f093d04c604411f0abf857
Khaledyoussef2003 <khaledyoussef2003@gmail.com>
2026-09-06 11:29:03 +0300

    Delete scripts directory

--- scripts/bootstrap_platforms.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# Run once on a machine with Flutter installed to generate native iOS/Android runners.
-flutter create --platforms=ios,android --org com.roamli .
-flutter pub get
